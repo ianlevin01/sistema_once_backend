50bcd44fb50deb155c033548ed330774c972fdec
ianlevin01 <ianlevin01@gmail.com>
2026-03-22 02:18:02 -0300

    a

--- src/configs/db-config.js
+++ src/configs/db-config.js
@@ -1,7 +1,7 @@
 const config = {
   host: "database-1.c3eyesi4ucpy.sa-east-1.rds.amazonaws.com",   // 🔥 endpoint de AWS
   user: "postgres",          // tu usuario
-  password: "dJuizitLudbM2Wt",   // tu password
+  password: "",   // tu password
   database: "postgres",      // o el nombre de tu DB
   port: 5432,               // ✅ PostgreSQL
 };
